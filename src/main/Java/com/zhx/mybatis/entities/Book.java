301d2f5f1ccbbecd5d1d1b3461df94c4f66ff17a
Vcy <Vcy>
2017-02-24 10:59:54 +0800

    逆向工程

---
+++ src/main/Java/com/zhx/mybatis/entities/Book.java
@@ -0,0 +1,43 @@
+package com.zhx.mybatis.entities;
+
+public class Book {
+    private String bookid;
+
+    private String bookname;
+
+    private String author;
+
+    private String price;
+
+    public String getBookid() {
+        return bookid;
+    }
+
+    public void setBookid(String bookid) {
+        this.bookid = bookid == null ? null : bookid.trim();
+    }
+
+    public String getBookname() {
+        return bookname;
+    }
+
+    public void setBookname(String bookname) {
+        this.bookname = bookname == null ? null : bookname.trim();
+    }
+
+    public String getAuthor() {
+        return author;
+    }
+
+    public void setAuthor(String author) {
+        this.author = author == null ? null : author.trim();
+    }
+
+    public String getPrice() {
+        return price;
+    }
+
+    public void setPrice(String price) {
+        this.price = price == null ? null : price.trim();
+    }
+}
